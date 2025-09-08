b5cf439cb71bb673ad2913812e0baeca6ada6073
pkay <kumarpramod66476@gmail.com>
2025-09-08 15:03:20 +0530

    GUI added

--- geometry/geometry.py
+++ geometry/geometry.py
@@ -1,4 +1,5 @@
 import math
+import turtle
 from random import randint as r
 class Point:
     def __init__(self, x, y):
@@ -19,8 +20,23 @@ class Rectangle:
         self.point1 = point1
         self.point2 = point2
 
+class GuiRectangle(Rectangle):
+    def draw(self, canvas):
+        canvas.penup()
+        canvas.goto(self.point1.x,self.point1.y)
+        canvas.pendown()
+        canvas.forward(self.point2.x - self.point1.x)
+        canvas.left(90)
+        canvas.forward(self.point2.y - self.point1.y)
+        canvas.left(90)
+        canvas.forward(self.point2.x - self.point1.x)
+        canvas.left(90)
+        canvas.forward(self.point2.y - self.point1.y)
+        canvas.left(90)
+
+
 print("Guess a point which lies in this Rectangle : ")
-r = Rectangle(Point(r(0,9),r(0,9)), Point(r(10,20),r(10,20)))
+r = GuiRectangle(Point(r(0,9),r(0,9)), Point(r(10,400),r(10,400)))
 print("Rectangle Coordinates : ", r.point1.x, r.point1.y, "and", r.point2.x, r.point2.y)
 
 up = Point(float(input("Guess X : ")), float(input("Guess Y : ")))
@@ -29,4 +45,12 @@ if up.falls_in_rectangle(r):
     print("You are right")
 else:
     print("You are wrong")
+
+canvas = turtle.Turtle()
+r.draw(canvas)
+canvas.penup()
+canvas.goto(up.x, up.y)
+canvas.pendown()
+turtle.done()
+
 
