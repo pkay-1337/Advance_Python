012e73a1a3027ddea08960b0df0caa876b3de954
pkay <kumarpramod66476@gmail.com>
2025-09-08 17:28:53 +0530

    PdfReport class

---
+++ App-2-Flatmates-Bill/report.py
@@ -0,0 +1,23 @@
+from fpdf import FPDF
+class PdfReport:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def generate_pdf(self, fm1, fm2, bill):
+        pdf = FPDF()
+        pdf.add_page()
+        pdf.set_font(family="Times", size=24)
+        pdf.image("./files/house.png", h=24)
+        pdf.cell(w=0, h=24, txt="Flatmate Bills!", border=1, ln=1, align="C")
+        pdf.set_font(family="Times", size=16)
+        pdf.cell(w=50, h=20, txt="Period", border=1, ln=0, align="C")
+        pdf.cell(w=0, h=20, txt=bill.period, border=1, ln=1, align="C")
+
+
+        pdf.cell(w=50, h=20, txt=fm1.name, border=1, ln=0, align="C")
+        pdf.cell(w=0, h=20, txt=f"{fm1.pays(bill)}", border=1, ln=1, align="C")
+        
+        pdf.cell(w=50, h=20, txt=fm2.name, border=1, ln=0, align="C")
+        pdf.cell(w=0, h=20, txt=f"{fm2.pays(bill)}", border=1, ln=1, align="C")
+        pdf.output(self.filename)
+
