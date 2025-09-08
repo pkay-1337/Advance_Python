9854ae54b9e859026d8367743ea6bc86eb3edcf4
pkay <kumarpramod66476@gmail.com>
2025-09-08 15:03:37 +0530

    Turtle test

---
+++ geometry/draw.py
@@ -0,0 +1,16 @@
+import turtle
+
+myturtle = turtle.Turtle()
+
+myturtle.penup()
+myturtle.goto(50,50)
+myturtle.pendown()
+myturtle.forward(200)
+myturtle.left(90)
+myturtle.forward(200)
+myturtle.left(90)
+myturtle.forward(200)
+myturtle.left(90)
+myturtle.forward(200)
+myturtle.left(90)
+turtle.done()
