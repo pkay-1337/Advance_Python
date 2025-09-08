395e633e9f11f90d9504b7685932dc83222f08b6
pkay <kumarpramod66476@gmail.com>
2025-09-08 15:44:23 +0530

    reated Bill class

---
+++ App-2-Flatmates-Bill/main.py
@@ -0,0 +1,6 @@
+class Bill:
+    """Object that contains data about bill. like period and amount"""
+    def __init__(self, amount, period):
+         self.amount = amount
+         self.period = period
+
