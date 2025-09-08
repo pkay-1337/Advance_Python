266cfa32ea5467f11070bc238daba8f05ad63241
pkay <kumarpramod66476@gmail.com>
2025-09-08 17:16:29 +0530

    Complete project

--- App-2-Flatmates-Bill/main.py
+++ App-2-Flatmates-Bill/main.py
@@ -1,3 +1,4 @@
+from fpdf import FPDF
 class Bill:
     """Object that contains data about bill. like period and amount"""
     def __init__(self, amount, period):
@@ -6,17 +7,42 @@ class Bill:
 
 class Flatmate:
     """flatmate"""
+    total_days = 0
     def __init__(self, name, days_in_house):
         self.name = name
         self.days_in_house = days_in_house
+        Flatmate.total_days = Flatmate.total_days + days_in_house
 
-    def pays(self):
-        pass
+    def pays(self, bill):
+        return (bill.amount/Flatmate.total_days) * self.days_in_house
 
 
 class PdfReport:
     def __init__(self, filename):
         self.filename = filename
 
-    def generate_pdf(self):
-        pass
+    def generate_pdf(self, fm1, fm2, bill):
+        pdf = FPDF()
+        pdf.add_page()
+        pdf.set_font(family="Times", size=24)
+        pdf.image("./files/house.png", h=24)
+        pdf.cell(w=0, h=24, txt="Flatmate Bills!", border=1, ln=1, align="C")
+        pdf.set_font(family="Times", size=16)
+        pdf.cell(w=50, h=20, txt="Period", border=1, ln=0, align="C")
+        pdf.cell(w=0, h=20, txt=bill.period, border=1, ln=1, align="C")
+
+
+        pdf.cell(w=50, h=20, txt=fm1.name, border=1, ln=0, align="C")
+        pdf.cell(w=0, h=20, txt=f"{fm1.pays(bill)}", border=1, ln=1, align="C")
+        
+        pdf.cell(w=50, h=20, txt=fm2.name, border=1, ln=0, align="C")
+        pdf.cell(w=0, h=20, txt=f"{fm2.pays(bill)}", border=1, ln=1, align="C")
+        pdf.output(self.filename)
+
+
+bill = Bill(amount=float(input("Input Bill amount : ")), period=input("Enter Period : "))
+fm1 = Flatmate(name=input("Enter Flatmate 1 name : "), days_in_house=int(input("His days in flat : ")))
+fm2 = Flatmate(name=input("Enter Flatmate 2 name : "), days_in_house=int(input("His days in flat : ")))
+report = PdfReport("flatmate_billing_report.pdf")
+report.generate_pdf(fm1, fm2, bill)
+printf("Billing Report PDF Generated")
